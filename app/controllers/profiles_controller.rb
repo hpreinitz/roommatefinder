cbcbce73cab88059486466210167956216b81bfd
H Preinitz <hpreinitz@gmail.com>
2014-04-10 12:07:14 -0500

    fix show and edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,6 +8,10 @@ class ProfilesController < ApplicationController
       set_profile
     end
 
+   def edit
+     @profile = Profile.find(params[:id])
+   end
+
     def destroy
       set_profile
       @profile.destroy
@@ -19,15 +23,13 @@ class ProfilesController < ApplicationController
 
     def update
 	  set_profile
-      respond_to do |format|
-        if @profile.update(profile_params)
-          format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
-          format.json { render action: 'show', status: :ok, location: @profile }
-        else
-          format.html { render action: 'edit' }
-          format.json { render json: @profile.errors, status: :unprocessable_entity }
-        end
-       end
+
+    if @profile.update(params[:profile].permit(:firstname, :lastname, :email, :birthdate, :gender, :bio, :interests))
+      redirect_to @profile
+    else
+      render 'edit'
+    end
+
     end
 
 	def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Roommatefinder::Application.routes.draw do
-  get "welcome/index"
+  
   resources :profiles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get "welcome/index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
