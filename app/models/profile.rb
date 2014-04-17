f9353edac56d8f4e409db41fb83fb5c6d5b940d3
H Preinitz <hpreinitz@gmail.com>
2014-04-17 16:04:08 -0500

    fixed checkboxes and where the value is displayed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,3 @@
 class Profile < ActiveRecord::Base
-	validates :firstname, presence: true, length: {minimum: 1}
-	validates :lastname, presence: true, length: {minimum: 1}
-	validates :password, confirmation: true
-	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
 
